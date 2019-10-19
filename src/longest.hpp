ae01484bb3a879b47407d05927d2367167a3e086
Dayu <commintern@hotmail.com>
2019-10-19 15:38:42 -0500

    Delete timerange

--- src/longest.hpp
+++ src/longest.hpp
@@ -2,6 +2,9 @@
 #define LONGEST_H
 
 #include <RcppArmadillo.h>
+
+
+
 arma::vec countprofun_C(const arma::vec& counttime, const arma::vec& externalTime);
 
 arma::cube Xgen_C(const arma::mat& covMat, const arma::vec& countprocess, const unsigned int& p);
